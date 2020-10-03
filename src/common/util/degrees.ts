3a4cbb049722a54265a26d63b73df65e1b3ff03d
Abbondanzo <peter@abbondanzo.com>
2020-10-03 15:35:09 -0400

    Only positive rotations

--- src/common/util/degrees.ts
+++ src/common/util/degrees.ts
@@ -1,7 +1,7 @@
 import { truncate } from "./numbers";
 
 export const degreesToRadians = (degrees: number) => {
-  return ((360 - degrees) * Math.PI) / 180;
+  return (((360 - degrees) % 360) * Math.PI) / 180;
 };
 
 export const radiansToDegrees = (radians: number) => {
